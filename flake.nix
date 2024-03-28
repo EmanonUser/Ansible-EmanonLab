afa750b9f83acc76ab7175a08da0dc685404a854
Emanon <moemanon@pm.me>
2024-03-28 11:28:47 +0100

    feat: add nix devShell

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Ansible development environment";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      pythonPackages = pkgs.python3Packages;
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pythonPackages;
+          [cryptography]
+          ++ [
+            pkgs.python3
+            pkgs.sshpass
+            pkgs.ansible
+          ];
+      };
+    });
+}
